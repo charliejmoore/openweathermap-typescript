84a377e22bbb615d495589c9bec4c461c2b47cd0
Charlie Moore <moorejchar@gmail.com>
2025-07-12 18:11:23 -0600

    🚨 Fix jest warnings

--- jest.config.js
+++ jest.config.js
@@ -3,11 +3,13 @@ export default {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
   extensionsToTreatAsEsm: ['.ts'],
-  globals: {
-    'ts-jest': {
-      useESM: true,
-      tsconfig: './tsconfig.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: './tsconfig.json',
+      },
+    ],
   },
   moduleNameMapper: {},
   testMatch: ['**/__tests__/**/*.test.ts'],
